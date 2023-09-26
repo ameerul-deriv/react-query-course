6510106252cb1c4b168b1a852a8c44d0c61c4869
ameerul hady <ameerul@deriv.com>
2023-09-26 15:08:58 +0800

    chore: added status, preloading, prefetching

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,17 +1,62 @@
-import { useQuery } from 'react-query';
-import IssueItem from './IssueItem';
+import { useState } from 'react';
+import { useQuery, useQueryClient } from 'react-query';
+import { IssueItem } from './IssueItem';
+import fetchWithError from '../helpers/fetchWithError';
+import Loader from './Loader';
 
-export default function IssuesList() {
-    const issuesQuery = useQuery(['issues'], () => fetch('/api/issues').then(res => res.json()));
+export default function IssuesList({ labels, status }) {
+    const queryClient = useQueryClient();
 
-    console.log(issuesQuery);
+    const issuesQuery = useQuery(['issues', { labels, status }], async ({ signal }) => {
+        const statusString = status ? `&status=${status}` : '';
+        const labelsString = labels.map(label => `labels[]=${label}`).join('&');
+        const results = await fetchWithError(`/api/issues?${labelsString}${statusString}`, {
+            signal,
+        });
+
+        results.forEach(issue => {
+            queryClient.setQueryData(['issues', issue.number.toString()], issue);
+        });
+
+        return results;
+    });
+    const [searchValue, setSearchValue] = useState('');
+
+    const searchQuery = useQuery(
+        ['issues', 'search', searchValue],
+        ({ signal }) => fetch(`/api/search/issues?q=${searchValue}`, { signal }).then(res => res.json()),
+        {
+            enabled: searchValue.length > 0,
+        }
+    );
 
     return (
         <div>
-            <h2>Issues List</h2>
+            <form
+                onSubmit={event => {
+                    event.preventDefault();
+                    setSearchValue(event.target.elements.search.value);
+                }}
+            >
+                <label htmlFor='search'>Search Issues</label>
+                <input
+                    type='search'
+                    placeholder='Search'
+                    name='search'
+                    id='search'
+                    onChange={event => {
+                        if (event.target.value.length === 0) {
+                            setSearchValue('');
+                        }
+                    }}
+                />
+            </form>
+            <h2>Issues List {issuesQuery.fetchStatus === 'fetching' ? <Loader /> : null}</h2>
             {issuesQuery.isLoading ? (
                 <p>Loading...</p>
-            ) : (
+            ) : issuesQuery.isError ? (
+                <p>{issuesQuery.error.message}</p>
+            ) : searchQuery.fetchStatus === 'idle' && searchQuery.isLoading === true ? (
                 <ul className='issues-list'>
                     {issuesQuery.data.map(issue => (
                         <IssueItem
@@ -27,6 +72,32 @@ export default function IssuesList() {
                         />
                     ))}
                 </ul>
+            ) : (
+                <>
+                    <h2>Search Results</h2>
+                    {searchQuery.isLoading ? (
+                        <p>Loading...</p>
+                    ) : (
+                        <>
+                            <p>{searchQuery.data.count} Results</p>
+                            <ul className='issues-list'>
+                                {searchQuery.data.items.map(issue => (
+                                    <IssueItem
+                                        key={issue.id}
+                                        title={issue.title}
+                                        number={issue.number}
+                                        assignee={issue.assignee}
+                                        commentCount={issue.comments.length}
+                                        createdBy={issue.createdBy}
+                                        createdDate={issue.createdDate}
+                                        labels={issue.labels}
+                                        status={issue.status}
+                                    />
+                                ))}
+                            </ul>
+                        </>
+                    )}
+                </>
             )}
         </div>
     );
